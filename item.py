29312e5622dd3460ba362cf54353c6e2aca19ff6
Juan López Rubio <j.lopez.r@gmail.com>
2024-02-16 22:32:12 +0100

    model metadata using annotations

--- item.py
+++ item.py
@@ -1,17 +1,5 @@
 from pydantic import BaseModel, Field
 
 class Item(BaseModel):
-    name: str = Field(..., description="The name of the item")
-    description: str = Field(None, description="The description of the item")
-
-    class Config:
-        json_schema_extra = {
-            "properties": {
-                "name": {
-                    "x-custom-field": "Custom value for name"
-                },
-                "description": {
-                    "x-custom-field": "Custom value for description"
-                },
-            }
-        }
+    name: str = Field(..., description="The name of the item", x_custom_other_field="Custom value for name", juan="HOLA")
+    description: str = Field(None, description="The description of the item", extra={"x-custom-other-field": "Custom value for descripton", "juan": "HOLA"})
